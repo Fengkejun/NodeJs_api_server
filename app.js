64a291074221c6526c1656a81bdcd8d271a28ca3
FengKeJun <2302970058@qq.com>
2025-12-29 13:44:40 +0800

    feat:登录注册-封装res.cc函数简化res.send方法

--- app.js
+++ app.js
@@ -11,9 +11,24 @@ app.use(cors());
 // 注意:只能解析`application/x-www-form-urlencoded` 格式的表单数据的中间件
 app.use(express.urlencoded({ extended: false }));
 
+// 封装响应数据的中间件
+app.use((req,res,next)=>{
+    // status 默认值为1，表示失败的情况
+    // err的值可能是一个错误对象，也可能是一个错误的描述字符串
+    res.cc = (err,status = 1)=>{
+        res.send({
+            status,
+            message: err instanceof Error ? err.message : err // 对err进行判断是否为错误对象或字符串
+        })
+    }
+    
+    next();                      
+})
+
 //导入并使用用户路由模块
 const userRouter = require('./router/user');
 app.use('/api', userRouter);
+
 
 
 // 启动服务器
--- router_handler/user.js
+++ router_handler/user.js
@@ -9,22 +9,25 @@ exports.regUser = ((req, res) => {
     const userInfo = req.body
     
     // 对用户信息进行合法性校验
-    if(!userInfo.username || !userInfo.password){
-        return res.send({status:1, message:'用户名或密码不合法或为空！'})
-    }
+    // if(!userInfo.username || !userInfo.password){
+    //     return res.send({status:1, message:'用户名或密码不合法或为空！'})
+    // }
+    if(!userInfo.username || !userInfo.password) return res.cc('用户名或密码不合法或为空！')
     // 验证用户名是否被注册
     // 定义SQL语句
     const sqlStr = 'select * from ev_users where username=?'
     db.query(sqlStr,[userInfo.username],(err,results) => {
         // 执行SQL语句失败
-        if(err){
-            console.log(err);
-            return res.send({status:1, message:err.message})
-        }
+        // if(err){
+        //     // console.log(err);
+        //     return res.send({status:1, message:err.message})
+        // }
+        if(err) return res.cc(err)
         // 用户名被占用
-        if(results.length > 0){
-            return res.send({status:1, message:'用户名被占用，请更换其他用户名！'})
-        }
+        // if(results.length > 0){
+        //     return res.send({status:1, message:'用户名被占用，请更换其他用户名！'})
+        // }
+        if(results.length > 0) return res.cc('用户名被占用，请更换其他用户名！')
         // 用户名可用
         // 调用bcrypt.hashSync()对密码进行加密
          userInfo.password = bcrypt.hashSync(userInfo.password,10)
@@ -33,13 +36,15 @@ exports.regUser = ((req, res) => {
         const sql = ' insert into ev_users set ?'
         db.query(sql,{username:userInfo.username,password:userInfo.password},(err,results) => {
             // 判断SQL语句是否执行成功
-            if(err)return res.send({status:1, message:err.message})
+            // if(err)return res.send({status:1, message:err.message})
+            if(err) return res.cc(err)
             // 判断影响行数是否为1
-        if(results.affectedRows !== 1)return res.send({status:1, message:'注册用户失败，请稍后再试！'})
-            // 注册成功
-        res.send({status:0, message:'注册成功！'})
-        })
-    // res.send('register OK');    
+        // if(results.affectedRows !== 1)return res.send({status:1, message:'注册用户失败，请稍后再试！'})
+        if(results.affectedRows !== 1) return res.cc('注册用户失败，请稍后再试！')
+        //     // 注册成功
+        // res.send({status:0, message:'注册成功！'})
+        res.cc('注册成功！',0)
+        })  
     })
 })
 
