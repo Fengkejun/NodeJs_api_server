b60c490a69babc68c7a25a24e04a22dc06845252
FengKeJun <2302970058@qq.com>
2025-12-29 11:21:58 +0800

    feat:登录注册-安装并配置mysql模块

---
+++ db/index.js
@@ -0,0 +1,11 @@
+// 下载并导入mysql模块
+const mysql = require('mysql');
+// 创建mysql连接池
+const db= mysql.createPool({
+    host: " 127.0.0.1",
+    user: "root",
+    password: "admin123",
+    database: "my_db_01"
+})
+
+module.exports = db;
