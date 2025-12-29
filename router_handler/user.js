a333bff7e8aea6253bfd34ff02707a5519b51859
FengKeJun <2302970058@qq.com>
2025-12-29 11:10:02 +0800

    feat:初始化项目-初始化用户路由模块

---
+++ router_handler/user.js
@@ -0,0 +1,11 @@
+// -----用户处理函数-----
+
+// 注册处理函数
+exports.regUser = ((req, res) => {
+    res.send('register OK');    
+})
+
+// 登录处理函数
+exports.login = ((req, res) => {
+    res.send('login OK');    
+})
