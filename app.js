b48082e02ba8b21bc40e4e8e3356e08c52905bde
FengKeJun <2302970058@qq.com>
2025-12-29 10:45:03 +0800

    feat:创建项目

---
+++ app.js
@@ -0,0 +1,13 @@
+// 导入express
+const express = require('express');
+// 创建express实例
+const app = express();
+
+
+
+
+
+// 启动服务器
+app.listen(3007, () => {
+  console.log('api server running at http://127.0.0.1:3007');
+});
