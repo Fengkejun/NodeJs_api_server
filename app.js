2be468321452111dd267b4393adb1e1738d3f4a3
FengKeJun <2302970058@qq.com>
2025-12-29 11:09:23 +0800

    feat:初始化项目

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const express = require('express');
 // 创建express实例
 const app = express();
 
+// 配置cors跨域
+const cors = require('cors');
+app.use(cors());
 
+// 配置解析表单数据的中间件
+// 注意:只能解析`application/x-www-form-urlencoded` 格式的表单数据的中间件
+app.use(express.urlencoded({ extended: false }));
 
+//导入并使用用户路由模块
+const userRouter = require('./router/user');
+app.use('/api', userRouter);
 
 
 // 启动服务器
